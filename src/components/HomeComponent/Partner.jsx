ee8487692c4622334386f5ef6ac329c73ee79d01
karmakarCoder <dev.rodro@gmail.com>
2025-09-27 20:53:26 +0600

    modified

--- src/components/HomeComponent/Partner.jsx
+++ src/components/HomeComponent/Partner.jsx
@@ -14,58 +14,60 @@ export default function Partner() {
         <h4 className="text-3xl lg:text-6xl font-bold lg:mb-5 text-[#8394a8]">
           Partners
         </h4>
-        <div>
-          <Splide
-            className="!px-0"
-            options={{
-              perPage: 4,
-              gap: "1rem",
-              arrows: false,
-              pagination: false,
-              autoplay: true,
-              breakpoints: {
-                1280: {
-                  perPage: 4,
-                  gap: "1rem",
-                },
-                1024: {
-                  perPage: 3,
-                  gap: ".8rem",
-                },
-                768: {
-                  perPage: 2,
-                  gap: ".7rem",
-                },
-                480: {
-                  perPage: 1,
-                  gap: ".5rem",
-                },
-              },
-            }}
-            aria-label="My Favorite Images"
-          >
-            {[...Array(10)].map((_, index) => (
-              <SplideSlide
-                key={index}
-                className={"hover:scale-105 duration-200"}
-              >
-                <div className="w-full  bg-[#f9f0cf] p-10 flex flex-col items-center justify-center gap-5">
-                  <h4
-                    className="text-3xl font-semibold text-black
-                 uppercase"
-                  >
-                    BMET
-                  </h4>
-                  <img src={logo1} alt="" className="w-36 lg:w-40" />
-                  <button className="w-full cursor-pointer hover:bg-primary hover:text-white   duration-200 py-2 text-primary bg-white rounded-md text-lg font-medium">
-                    Go to website
-                  </button>
-                </div>
-              </SplideSlide>
-            ))}
-          </Splide>
-        </div>
       </Container>
+      <div>
+        <Splide
+          className="!px-0 !py-0 h-[450px]"
+          options={{
+            perPage: 4,
+            gap: "1rem",
+            arrows: false,
+            pagination: false,
+            padding: "5rem",
+            autoplay: true,
+            breakpoints: {
+              1280: {
+                perPage: 4,
+                gap: "1rem",
+              },
+              1024: {
+                perPage: 3,
+                gap: ".8rem",
+              },
+              768: {
+                perPage: 2,
+                gap: ".7rem",
+              },
+              480: {
+                perPage: 1,
+                gap: "1rem",
+                padding: "3rem",
+              },
+            },
+          }}
+          aria-label="My Favorite Images"
+        >
+          {[...Array(10)].map((_, index) => (
+            <SplideSlide
+              key={index}
+              className={"hover:scale-105 duration-200 py-10"}
+            >
+              <div className="w-full  bg-[#f9f0cf] rounded-2xl p-10 flex flex-col items-center justify-center gap-5">
+                <h4
+                  className="text-3xl font-semibold text-black
+                 uppercase"
+                >
+                  BMET
+                </h4>
+                <img src={logo1} alt="" className="w-36 lg:w-40" />
+                <button className="w-full cursor-pointer hover:bg-primary hover:text-white   duration-200 py-2 text-primary bg-white rounded-md text-lg font-medium">
+                  Go to website
+                </button>
+              </div>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
     </div>
   );
 }
